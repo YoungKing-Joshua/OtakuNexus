dc603f48a0d476ac63986ef38d70bfe28b7d8a86
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2024-06-06 15:53:18 +0000

    Otaku Nexus

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -9,88 +9,85 @@ import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
 const SinglePage = ({ params }) => {
-  const { slug } = params;
-  const [data, setData] = useState(null);
-  const { data: session, status } = useSession();
-  const router = useRouter();
+	const { slug } = params;
+	const [data, setData] = useState(null);
+	const { data: session, status } = useSession();
+	const router = useRouter();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(`/api/posts/${slug}`, { cache: "no-store" });
-        if (!res.ok) throw new Error("Failed to fetch post data");
-        const postData = await res.json();
-        setData(postData);
-      } catch (err) {
-        console.error(err);
-      }
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await fetch(`/api/posts/${slug}`, { cache: "no-store" });
+				if (!res.ok) throw new Error("Failed to fetch post data");
+				const postData = await res.json();
+				setData(postData);
+			} catch (err) {
+				console.error(err);
+			}
+		};
 
-    fetchData();
-  }, [slug]);
+		fetchData();
+	}, [slug]);
 
-  const handleDelete = async () => {
-    try {
-      const res = await fetch(`/api/posts/${slug}`, { method: "DELETE" });
-      if (res.status === 204) {
-        router.push("/"); 
-      } else {
-        console.error("Failed to delete post");
-      }
-    } catch (error) {
-      console.error("Error deleting post:", error);
-    }
-  };
+	const handleDelete = async () => {
+		try {
+			const res = await fetch(`/api/posts/${slug}`, { method: "DELETE" });
+			if (res.status === 204) {
+				router.push("/");
+			} else {
+				console.error("Failed to delete post");
+			}
+		} catch (error) {
+			console.error("Error deleting post:", error);
+		}
+	};
 
-  if (status === "loading" || !data) {
-    return <div>Loading...</div>;
-  }
+	if (status === "loading" || !data) {
+		return <div>Loading...</div>;
+	}
 
-  const isAuthor = session?.user?.email === data.userEmail;
-  const createdDate = data?.createdAt.substring(0, 10);
+	const isAuthor = session?.user?.email === data.userEmail;
+	const createdDate = data?.createdAt.substring(0, 10);
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.infoContainer}>
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}>{data?.title}</h1>
-          <div className={styles.user}>
-            {data?.user?.image && (
-              <div className={styles.userImageContainer}>
-                <Image src={data.user.image} alt="" fill className={styles.avatar} />
-              </div>
-            )}
-            <div className={styles.userTextContainer}>
-              <span className={styles.username}>{data?.user.name}</span>
-              <span className={styles.date}>{createdDate}</span>
-            </div>
-          </div>
-        </div>
-        {data?.img && (
-          <div className={styles.imageContainer}>
-            <Image src={data.img} alt="" fill className={styles.image} />
-          </div>
-        )}
-      </div>
-      <div className={styles.content}>
-        <div className={styles.post}>
-          <div
-            className={styles.description}
-            dangerouslySetInnerHTML={{ __html: data?.desc }}
-          />
-          {isAuthor && (
-            <button className={styles.deleteButton} onClick={handleDelete}>
-              Delete Post
-            </button>
-          )}
-          <div className={styles.comment}>
-            <Comments postSlug={slug} />
-          </div>
-        </div>
-        <Menu />
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<div className={styles.infoContainer}>
+				<div className={styles.textContainer}>
+					<div className={styles.user}>
+						{data?.user?.image && (
+							<div className={styles.userImageContainer}>
+								<Image src={data.user.image} alt='' fill className={styles.avatar} />
+							</div>
+						)}
+						<div className={styles.userTextContainer}>
+							<span className={styles.username}>{data?.user.name}</span>
+							<span className={styles.date}>{createdDate}</span>
+						</div>
+					</div>
+					<h1 className={styles.title}>{data?.title}</h1>
+				</div>
+			</div>
+			<div className={styles.content}>
+				<div className={styles.post}>
+					{data?.img && (
+						<div className={styles.imageContainer}>
+							<Image src={data.img} alt='' fill className={styles.image} />
+						</div>
+					)}
+					<div className={styles.description} dangerouslySetInnerHTML={{ __html: data?.desc }} />
+					{isAuthor && (
+						<button className={styles.deleteButton} onClick={handleDelete}>
+							Delete Post
+						</button>
+					)}
+					<div className={styles.comment}>
+						<Comments postSlug={slug} />
+					</div>
+				</div>
+				<Menu />
+			</div>
+		</div>
+	);
 };
 
 export default SinglePage;
